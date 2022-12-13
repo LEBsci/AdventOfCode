13b656e09d44b26b1765ee61321ff87944c23107
LEBsci <80007906+LEBsci@users.noreply.github.com>
2022-12-13 18:56:08 +0100

    Day 6 solution 2 and day 7 folder

--- Day_06/parse.py
+++ Day_06/parse.py
@@ -5,3 +5,8 @@ for i in range(len(stream)-3): # Iterate over every character in packs of four
     if len(set(stream[i:i+4])) == 4: # Check for a set in which there are four different symbols
         print('A total of ' + str(i+4) + ' characters need to be processed before the first start-of-packet marker is detected')
         break # Stop checking after the first packet appears
+        
+for i in range(len(stream)-13): # Iterate over every character in packs of fourteen
+    if len(set(stream[i:i+14])) == 14: # Check for a set in which there are four different symbols
+        print('A total of ' + str(i+14) + ' characters need to be processed before the first start-of-message marker is detected')
+        break # Stop checking after the first packet appears 
